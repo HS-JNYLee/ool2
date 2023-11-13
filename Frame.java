0cdc7ea54223541ac38b3818ec6199d9e7db5f0d
jihun <gnswlwjs777@hansung.ac.kr>
2023-11-13 23:40:20 +0900

    frame 좌표값으로 변경..

--- Frame.java
+++ Frame.java
@@ -6,39 +6,29 @@ public class Frame extends JFrame {
     public Frame() {
         super("Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel label = new JLabel("");
-        getContentPane().add(label, BorderLayout.NORTH);
+        setLayout(null);  // 레이아웃 매니저를 null로 설정
+
+        JLabel label = new JLabel("Character");
+        label.setBounds(100, 100, 100, 30);  // 레이블의 위치와 크기 설정
+        add(label);
 
         // 설정 버튼 생성
         JButton settingButton = new JButton("설정");
-        // 버튼의 크기 설정
-        Dimension settingButtonSize = new Dimension(60, 1);
-        settingButton.setPreferredSize(settingButtonSize);
-        // 왼쪽 상단에 설정 버튼 추가
-        getContentPane().add(settingButton, BorderLayout.WEST);
+        settingButton.setBounds(1, 1, 60, 30);  // 버튼의 위치와 크기 설정
+        add(settingButton);
 
-        // 버튼을 담을 패널 생성
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(1, 3, 20, 0)); // 간격을 조절하는 부분
-
-        // 각각의 버튼 생성
+        // 버튼 생성 및 배치
         JButton button1 = new JButton("식량");
+        button1.setBounds(10, 360, 200, 70);  // 버튼의 위치와 크기 설정
+        add(button1);
+
         JButton button2 = new JButton("물");
+        button2.setBounds(300, 360, 200, 70);  // 버튼의 위치와 크기 설정
+        add(button2);
+
         JButton button3 = new JButton("무기");
-
-        // 각 버튼의 크기 설정
-        Dimension buttonSize = new Dimension(260, 80);
-        button1.setPreferredSize(buttonSize);
-        button2.setPreferredSize(buttonSize);
-        button3.setPreferredSize(buttonSize);
-
-        // 패널에 각 버튼 추가
-        buttonPanel.add(button1);
-        buttonPanel.add(button2);
-        buttonPanel.add(button3);
-
-        // 창의 SOUTH에 패널 추가
-        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        button3.setBounds(580, 360, 200, 70);  // 버튼의 위치와 크기 설정
+        add(button3);
 
         // 창 크기 설정
         setSize(800, 500);
