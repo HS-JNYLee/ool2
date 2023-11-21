15b55196fa8531f99f5048b6ace81059703d1a63
jihun <gnswlwjs777@hansung.ac.kr>
2023-11-21 23:16:02 +0900

    맵추가

--- Map.java
+++ Map.java
@@ -1,3 +1,27 @@
-public class Map 
-{
+//서울, 부천, 수원, 평택, 대전, 대구, 부산, 제주도
+public class Map {
+    private String name;  // 지역 이름
+    private Mon monster;  // 해당 지역의 몬스터
+
+    public Map(String name, Mon monster) {  // 맵 생성 (지역 이름, 몬스터)
+        this.name = name;
+        this.monster = monster;
+    }
+
+    // getter & setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Mon getMonster() {
+        return monster;
+    }
+
+    public void setMonster(Mon monster) {
+        this.monster = monster;
+    }
 }
