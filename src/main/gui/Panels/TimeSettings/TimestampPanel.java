65c03e9d9bae7b928afb9ec042fd33398117b693
JNYLee <2071013@hansung.ac.kr>
2023-12-11 14:55:03 +0900

    [feature]: 날짜, 시간 갱신 thread 추가

--- src/main/gui/Panels/TimeSettings/TimestampPanel.java
+++ src/main/gui/Panels/TimeSettings/TimestampPanel.java
@@ -1,6 +1,7 @@
 package src.main.gui.Panels.TimeSettings;
 
 import src.main.app.common.CommonPanelFunction;
+import src.main.environment.TimestampThread;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,18 +16,20 @@ public class TimestampPanel extends JPanel {
         this.width = width;
         this.height = height;
 
-        region = new TimestampLabel("서울");
+        region = new TimestampLabel("서울"); // 시작 지역
         region.setForeground(CommonPanelFunction.hexToRgb("856b54"));
-        day = new TimestampLabel("Day 02");
-        realtime = new TimestampLabel("21:00");
+        day = new TimestampLabel("Day 01"); // 시작 날짜
+        realtime = new TimestampLabel("");
         realtime.setForeground(CommonPanelFunction.hexToRgb("D0D0D0"));
+        TimestampThread tt = new TimestampThread(realtime, day); // 날짜, 시간 갱신
+        tt.start(); // 시간 스레드 시작
 
         add(region);
         add(day);
         add(realtime);
     }
 
-    class TimestampLabel extends JLabel {
+    public class TimestampLabel extends JLabel {
         TimestampLabel(String name) {
             Font f = new Font("NanumGothic", Font.ITALIC, (int) (height * 0.5));
 
