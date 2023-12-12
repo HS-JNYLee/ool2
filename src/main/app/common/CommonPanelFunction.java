39257ad3502492c3bbbb7514d6b595271518c3f7
JNYLee <2071013@hansung.ac.kr>
2023-12-12 22:31:24 +0900

    [feature]: 아이템 고유 ID(uuid) 추가

--- src/main/app/common/CommonPanelFunction.java
+++ src/main/app/common/CommonPanelFunction.java
@@ -2,6 +2,7 @@ package src.main.app.common;
 
 import java.awt.*;
 import java.util.Random;
+import java.util.UUID;
 
 public class CommonPanelFunction {
     static public Color hexToRgb(String hexCode) {
@@ -15,5 +16,10 @@ public class CommonPanelFunction {
     public static boolean getRandomBoolean(double probability) {
         Random random = new Random();
         return random.nextDouble() < probability;
+    }
+
+    // 랜덤 UUID 생성 하는 코드 : 아이템 식별용
+    public static String getRandomId() {
+        return  UUID.randomUUID().toString();
     }
 }
--- src/main/inventory/Item.java
+++ src/main/inventory/Item.java
@@ -1,5 +1,7 @@
 package src.main.inventory;
 
+import src.main.app.common.CommonPanelFunction;
+
 /**
  * 작성자 : JNYLee
  * 마지막 수정 : 23.11.13
@@ -7,6 +9,7 @@ package src.main.inventory;
  **/
 
 public class Item {
+    private String id;
     private String imgLink; // 아이템의 이미지 주소를 저장할 변수
 
     public String getImgLink() {
@@ -15,5 +18,17 @@ public class Item {
 
     public void setImgLink(String imgLink) {
         this.imgLink = imgLink;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Item() {
+        this.setId(CommonPanelFunction.getRandomId());
     }
 }
