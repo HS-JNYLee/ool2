b2e7bb2576c7f86b93dba17f5a616ed6bf2c0c3e
jihun <gnswlwjs777@hansung.ac.kr>
2023-12-13 14:06:35 +0900

    게임 설명 패널

--- src/main/gui/Panels/ExplainPanel/ExplainPanel.java
+++ src/main/gui/Panels/ExplainPanel/ExplainPanel.java
@@ -15,8 +15,8 @@ public class ExplainPanel extends JPanel {
 
         // 각 페이지를 나타내는 패널들을 생성
         JPanel page1 = createPage("", "src/resources/images/explain1.png");
-        JPanel page2 = createPage("Page 2 Content", "explain2.png");
-        JPanel page3 = createPage("Page 3 Content", "explain3.png");
+        JPanel page2 = createPage("", "src/resources/images/explain.png");
+        JPanel page3 = createPage("", "src/resources/images/explain3.png");
 
         // 각 페이지를 cardPanel에 추가
         cardPanel.add(page1, "Page 1");
